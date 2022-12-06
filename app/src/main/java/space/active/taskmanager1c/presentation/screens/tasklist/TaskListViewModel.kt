f6ea17b280b72d7bd8ba8d52eaeb18333ffc0ff5
NlightN22 <oncharterliz@gmail.com>
2022-12-07 02:20:57 +0700

    add style for title card view add style for expanded add task detailed spent very much time to find good button with editable background and stroke - that is MaterialButton!

---
+++ app/src/main/java/space/active/taskmanager1c/presentation/screens/tasklist/TaskListViewModel.kt
@@ -0,0 +1,17 @@
+package space.active.taskmanager1c.presentation.screens.tasklist
+
+import androidx.lifecycle.ViewModel
+
+class TaskListViewModel: ViewModel() {
+    // get tasklist
+    // open taskedit
+    // open newtask
+    // get ordered tasklit
+    // get filtered tasklist
+    // set task as completed
+    // set task as not_completed
+    // set task as unreadable
+    // goBack
+
+
+}
